f1d93ecc26ceab4d9e268af180c21216fdfcc88f
Sam Baker <Sam.Baker101@gmail.com>
2022-01-21 13:51:22 -0500

    ...

--- tcode/motor_drive_t.cpp
+++ tcode/motor_drive_t.cpp
@@ -92,8 +92,8 @@ static int PWMPeriod(int pin){
 		return -1;
 	}
 
-	if (-1 == write(fd, (const void*)&period_ns, sizeof(period_ns))){
-		fprintf(stderr, "Failed to set period for %d", pin);
+	if (-1 == write(fd, &period_ns, sizeof(period_ns))){
+		fprintf(stderr, "Failed to set period for %d \n", pin);
 		printf("ERROR: %d \n", errno);
 	}
 
