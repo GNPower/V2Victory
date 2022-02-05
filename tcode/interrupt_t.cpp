3222ae80b7f018e3f056cee85a44ba64549e34e2
Sam Baker <Sam.Baker101@gmail.com>
2022-02-05 17:47:33 -0500

    Update interrupt_t.cpp

--- tcode/interrupt_t.cpp
+++ tcode/interrupt_t.cpp
@@ -1,12 +1,44 @@
 #include "motor_drive.h"
+#include "localize.h"
 #include <poll.h>
+#include <pthread.h>
+
+void* counter(void*){
+	while(1){
+		pfd.fd = fd;
+		pfd.events = POLLPRI;
+
+		lseek(fd, 0, SEEK_SET);
+		read(fd, buf, sizeof buf);
+		poll(&pfd, 1, -1);
+
+
+		lseek(fd, 0, SEEK_SET);
+		read(fd, buf, sizeof buf);
+		count ++;
+		printf("COUNT: %d \n", count);
+
+		if (count == 20)
+			break;
+
+	}
+}
+
 
 int main(){
+	pthread_t tid;
 	char str[256];
 	int fd;
 	struct pollfd pfd;
 	char buf[8];
 	int count = 0;
+
+	if (-1 == GPIOExport(STOP))
+		return 1;
+
+	if (-1 == GPIODirection(STOP, IN))
+	return 2;
+
 
 	if (-1 == GPIOExport(LENCODER))
 		return 1;
@@ -26,24 +58,15 @@ int main(){
 		printf("/sys/class/gpio/gpio%d/value \n", LENCODER);
 		return -1;
 	}
+
+	pthread_create(&tid, NULL, &count, NULL);
+
 	while(1){
-		pfd.fd = fd;
-		pfd.events = POLLPRI;
-
-		lseek(fd, 0, SEEK_SET);
-		read(fd, buf, sizeof buf);
-		poll(&pfd, 1, -1);
-
-
-		lseek(fd, 0, SEEK_SET);
-		read(fd, buf, sizeof buf);
-		count ++;
-		printf("COUNT: %d \n", count);
-
-		if (count == 20)
-			break;
+		if(GPIORead(STOP)) break;
 	}
 
+
+	pthread_join(&tid, NULL);
 
 	if (-1 == GPIOUnexport(LENCODER))
 		return 1;
