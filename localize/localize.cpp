99b2d7952d28bac27d1d77ceefd1f835593dcd82
Sam Baker <Sam.Baker101@gmail.com>
2022-03-24 17:43:42 -0400

    Update localize.cpp

--- localize/localize.cpp
+++ localize/localize.cpp
@@ -36,15 +36,16 @@ inline float get_y_distance(float current_y, float target_y){
 
 void update_location(Vehicle_Data *ego, uint32_t x_distance, uint32_t y_distance){
 	ego->position_x += x_distance;
-	ego->position_y += y_distance
+	ego->position_y += y_distance;
 	}
 
 
 void get_abs_distance(Vehicle_Data *ego, Intersection_Data *target, float* x_y_distance[]){
-	float x_distance = get_x_distance(current_x, target_x);
-	float y_distance = get_x_distance(current_y, target_y);
-	float abs_distance = sqrt((x_distance*x_distance)+(y_distance*y_distance));
-	return abs_distance;
+	//float x_distance = get_x_distance(current_x, target_x);
+	//float y_distance = get_x_distance(current_y, target_y);
+	//float abs_distance = sqrt((x_distance*x_distance)+(y_distance*y_distance));
+	//return abs_distance;
+	continue;
 	} 
 
 
