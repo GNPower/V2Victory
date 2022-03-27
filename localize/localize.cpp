5cc04c6c7e3cd8ec6ad7aa74a8d49c926387b4e8
Sam Baker <Sam.Baker101@gmail.com>
2022-03-27 14:52:32 -0400

    Update localize.cpp

--- localize/localize.cpp
+++ localize/localize.cpp
@@ -36,8 +36,8 @@ void get_direction(Vehicle_Data *ego, Intersection_Data *target, float* directio
 
 void get_speed(Vehicle_Data *ego, float timestep){
 	float x_distance, y_distance, abs_distance;
-	get_x_distance_traveled(&ego, &x_distance);
-	get_y_distance_traveled(&ego, &y_distance);
+	get_x_distance_traveled(ego, &x_distance);
+	get_y_distance_traveled(ego, &y_distance);
 	abs_distance = sqrt(x_distance*x_distance + y_distance*y_distance);
 	ego->speed = (uint32_t)(abs_distance/timestep);
 }
