a7233bd41fec42582f6541d5546b8a71559143e2
Brennan David <davidb3@mcmaster.ca>
2022-04-03 17:20:04 -0400

    Remove versioned build files

--- vehicle_main/build/vehicle_main/CMakeFiles/vehicle_main.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vehicle_main"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vehicle_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- vehicle_main/build/vehicle_main/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(vehicle_main_IDL_FILES "msg/Car.idl;msg/Intersection.idl")
-set(vehicle_main_INTERFACE_FILES "msg/Car.msg;msg/Intersection.msg")
