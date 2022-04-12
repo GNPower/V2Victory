f89ca2d7c4eb998e3a6ed7a9c1bb796d40bb008a
johnsonqu1999 <johnsonqu1999@gmail.com>
2022-04-12 05:49:51 -0400

    get_int_accel_req signal stopping code

--- vehicle.cpp
+++ vehicle.cpp
@@ -670,8 +670,8 @@ double vehicle::get_lead_accel_req(void)
 
     double desired_speed;
 
-    // iError = iError + dist_error*0.0001;
-    iError = dist_error*0.1;
+    iError = iError + dist_error*0.00001;
+    // iError = dist_error*0.1;
     std::cout << "iError = " << iError << "\n";
 
     if(abs(dist_error) <= 0.01)
@@ -691,7 +691,7 @@ double vehicle::get_lead_accel_req(void)
         Speed should be 0 if we are on top of the car
         Simply scale linearly
         */
-        desired_speed =  (coasted_ego_speed + lead_rel_vel_x)*(1 + dist_error/desired_lead_gap) + iError;
+        desired_speed =  (coasted_ego_speed + lead_rel_vel_x)*(1 + 2*dist_error/desired_lead_gap) + iError;
         //dist_error -ve so ends up being 1 - fraction, so lower than set speed
 
         #ifdef DEBUG
@@ -764,6 +764,12 @@ double vehicle::get_int_accel_req(void)
             //using: delta_s = vi*delta_t + a/2*delta_t^2
             //add safety factor to tts to ensure not entering on red light
             double safe_tts = int_time_to_switch + TTS_SAFETY_FACTOR;
+
+            // Equations derived using (int_ent_dist - k_front_pos) = integral[0->safe_tts](v_initial + a*t), solving for a
+            int_accel_req = (2/(safe_tts*safe_tts))*((int_ent_dist - k_front_pos) - coasted_ego_speed*safe_tts);
+
+            /*
+            // Old way
             double make_light_req = (int_ent_dist - k_front_pos - coasted_ego_speed*safe_tts)*2/pow(safe_tts, 2);
 
             //need to ensure we dont go over set speed, so compare vf with set speed 
@@ -782,7 +788,7 @@ double vehicle::get_int_accel_req(void)
             {
                 //request is below set speed at entrance, ok to use
                 int_accel_req = make_light_req;
-            }
+            }*/
         }
         else
         {
@@ -866,6 +872,8 @@ double vehicle::get_int_accel_req(void)
     #ifdef DEBUG
     std::cout << "get_int_accel_req: int_accel_req = " << int_accel_req << "\n";
     #endif
+
+    int_accel_req = fmin(MAX_ACCEL, int_accel_req);
 
     return int_accel_req;
 }
